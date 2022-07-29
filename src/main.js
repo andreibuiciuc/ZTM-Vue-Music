5a6c649a6fefe789dea08255c395e9e3a716da18
Andrei Buiciuc <abuiciuc33@gmail.com>
2022-07-29 21:38:28 +0300

    assets and state configuration done + auth modal templates

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
 import App from './App.vue';
 import router from './router';
-import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+import './assets/tailwind.css';
+import './assets/main.css';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
