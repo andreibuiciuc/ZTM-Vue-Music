137a0519c3f754f805b06d4c7cc5abd5cdcb6e07
Andrei Buiciuc <abuiciuc33@gmail.com>
2022-08-06 22:45:20 +0300

    routing and guards done

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router';
-// import HomeView from '../views/HomeView.vue';
+import useUserStore from '@/store/user';
+
+import HomeView from '@/views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
+import ManageView from '@/views/ManageView.vue';
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView,
-  // },
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: AboutView
   },
+  {
+    path: '/manage-music',
+    name: 'manage',
+    component: ManageView,
+    beforeEnter: (_to, _from, next) => {
+      next();
+    },
+    meta: {
+      authenticationRequired: true
+    }
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500'
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.authenticationRequired) {
+    next();
+    return;
+  }
+
+  const store = useUserStore();
+
+  if (store.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
 });
 
 export default router;
